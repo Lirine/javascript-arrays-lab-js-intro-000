d9edbf0bba5abc87e3a45128c141077b516500f2
modern-integer-2602 <iloikov@gmail.com>
2021-02-20 03:38:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+function appendKitten(name) {
+  const kittens2 = [name, ...kittens]
+}
