7fbfc119b3e19d986ec100393f104a8b20bec186
modern-integer-2602 <iloikov@gmail.com>
2021-02-20 03:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift();
 }
 function appendKitten(name) {
-  const kittens2 = [name, ...kittens];
+  const kittens2 = [...kittens, name];
   return kittens2;
 }
